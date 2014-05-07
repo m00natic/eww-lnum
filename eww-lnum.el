d7a771a2d80a6175caecd38fefa81e70d77b13c3
Andrey Kotlarski <m00naticus@gmail.com>
2014-05-07 22:09:02 +0300

    Add package-requires declaration.

--- eww-lnum.el
+++ eww-lnum.el
@@ -6,6 +6,7 @@
 ;; Keywords: eww, browse, conkeror
 ;; Author: Andrey Kotlarski <m00naticus@gmail.com>
 ;; URL: https://github.com/m00natic/eww-lnum
+;; Package-Requires ((emacs "24.4"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
